6a4f72e0bb0e887b64e812e8f2f8c110889b7b05
Yulia <uliahoodzov@gmail.com>
2023-07-27 00:48:58 +0300

    full get Cesar shifr

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isValidPath = false;
@@ -13,11 +14,11 @@ public class Main {
         System.out.println("Добрий день! Введіть будь ласка шлях до вашохо файлу");
         String path = scanner.nextLine();
 
-        while(!isValidPath) {
+        while (!isValidPath) {
             if (isValidFilePath(path)) {
                 isValidPath = true;
                 readFile(path);
-            }else {
+            } else {
                 System.out.println("Шлях до файлу не вірний( спробуйте будь ласка ще раз!");
                 path = scanner.nextLine();
             }
@@ -36,8 +37,6 @@ public class Main {
             }
         }
 
-
-
         System.out.println("Оберіть що саме ви хочете зробити з данними файлу :\n" +
                 "Якщо ви хочете зашифрувати данні шифром Цезаря , введіть " + "+\n" +
                 "Якщо ви хочете розшифрувати данні шифром Цезаря , введіть " + "-");
@@ -46,38 +45,36 @@ public class Main {
         while (!cheak) {
             String userSign = scanner.nextLine();
             if (userSign.equals("+")) {
-                encrypt(path, key, scanner);
+                encrypt(path, key);
                 cheak = true;
             } else if (userSign.equals("-")) {
-
+                decrypt(path, key);
+                cheak = true;
             } else {
                 System.out.println("Введіть вірний символ!");
-                //userSign = scanner.nextLine();
             }
         }
     }
 
-    public static void encrypt(String path , int key , Scanner scanner){
+    public static void encrypt(String path, int key) {
         char[] elementsOfAlphabetic = new char[]{
                 'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є',
                 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л',
                 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У',
                 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю',
-                'Я', ':', '!', '?', ',', '.', '\"', ' ',
-                'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є',
+                'Я', 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є',
                 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л',
                 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
                 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю',
-                'я'};
-
+                'я', ':', '!', '?', ',', '.', '\"', ' ',};
 
         String FilePath = "encrypted.txt";
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(FilePath))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(FilePath))) {
             String linen = bufferedReader.readLine();
             char[] charArray = linen.toCharArray();
-            for (int i = 0; i < linen.length(); i++){
-                int index = search(elementsOfAlphabetic,charArray[i]);
+            for (int i = 0; i < linen.length(); i++) {
+                int index = search(elementsOfAlphabetic, charArray[i]);
                 int newIndex = (index + key) % elementsOfAlphabetic.length;
                 String newChar = String.valueOf(elementsOfAlphabetic[newIndex]);
                 bufferedWriter.write(newChar);
@@ -86,9 +83,44 @@ public class Main {
             bufferedWriter.close();
             readNAme(FilePath);
         } catch (IOException e) {
-            System.out.println("О ні! Виникла помилка");;
+            e.printStackTrace();
         }
     }
+
+    public static void decrypt(String path, int key) {
+        char[] elementsOfAlphabetic = new char[]{
+                'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є',
+                'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л',
+                'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У',
+                'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю',
+                'Я', 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є',
+                'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л',
+                'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+                'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю',
+                'я', ':', '!', '?', ',', '.', '\"', ' ',};
+
+        String FilePath = "decrypted.txt";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(FilePath))) {
+            String lines = bufferedReader.readLine();
+            char[] charArray = lines.toCharArray();
+            for (int i = 0; i < lines.length(); i++) {
+                int index = search(elementsOfAlphabetic, charArray[i]);
+                int newIndex = (index - key) % elementsOfAlphabetic.length;
+                if (newIndex <= 0) {
+                    newIndex = (index - key + elementsOfAlphabetic.length) % elementsOfAlphabetic.length;
+                }
+                String newChar = String.valueOf(elementsOfAlphabetic[newIndex]);
+                bufferedWriter.write(newChar);
+            }
+            bufferedReader.close();
+            bufferedWriter.close();
+            readNAme(FilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static boolean isValidFilePath(String filePath) {
         try {
             Path path = Paths.get(filePath);
@@ -97,7 +129,8 @@ public class Main {
             return false;
         }
     }
-    public static void readFile(String path){
+
+    public static void readFile(String path) {
         try {
             File file = new File(path);
             BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -110,7 +143,8 @@ public class Main {
             e.printStackTrace();
         }
     }
-    public static void readNAme(String newNAme){
+
+    public static void readNAme(String newNAme) {
         try {
             File file = new File(newNAme);
             BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -123,7 +157,8 @@ public class Main {
             e.printStackTrace();
         }
     }
-    public static int search(char[] alpha, char target){
+
+    public static int search(char[] alpha, char target) {
         for (int i = 0; i < alpha.length; i++) {
             if (alpha[i] == target) {
                 return i;
